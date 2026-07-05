b97eab926356b26d5d7674823dcd9dbf46d4cfc2
Rxnxld <102249404+Rxnxld@users.noreply.github.com>
2026-07-05 02:00:10 -0500

    Delete iniciar.bat

--- iniciar.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd /d "%~dp0"
-echo ========================================
-echo   Grupo Calle Portoviejo - Iniciando...
-echo ========================================
-echo.
-python -X utf8 app.py
-pause
